d054b61229f67b68f62b3ceb9d9f43476597085f
GEETHA.A <106382156+75520811@users.noreply.github.com>
2022-06-27 13:36:07 +0530

    Add files via upload

---
+++ selenium.java
@@ -0,0 +1,13 @@
+package project1;
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebElement;
+import org.
+
+public class selenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
